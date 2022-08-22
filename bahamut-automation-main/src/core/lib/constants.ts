ade3e218ccbb64f9daebbc268b8f9fa6f3986add
NEXTFLACON <92746449+NEXTFLACON@users.noreply.github.com>
2022-08-22 22:35:09 +0800

    Delete bahamut-automation-main directory

--- bahamut-automation-main/src/core/lib/constants.ts
+++
@@ -1,8 +0,0 @@
-export const LINK = {
-    DISCORD: "https://discord.gg/Ff8q2SGut2",
-    GITLAB: "https://gitlab.com/JacobLinCool/bahamut-automation",
-} as const;
-
-export const BRWOSER_TYPES = ["chromium", "firefox", "webkit"] as const;
-
-export const VERBOSE = process.env.VERBOSE ? true : false;
--- bahamut-automation-main/src/core/lib/module-utils/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./goto.js";
-export * from "./template.js";
-export * from "./time.js";
-export * from "./user.js";
-export * from "./cloudflare.js";
-export { booleanify } from "../utils.js";
